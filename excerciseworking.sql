8f59ab5c7c255dbf96d50e02f334debd845cc146
Trouperton <58820346+Trouperton@users.noreply.github.com>
2025-11-10 19:54:06 +0000

    Followed half of chapter 1 of the course.

--- excerciseworking.sql
+++ excerciseworking.sql
@@ -1,1 +1,15 @@
-SELECT * FROM people;
+--SELECT * FROM people;
+--SELECT 'Hello, World!';
+--SELECT first_name FROM people;
+--SELECT last_name FROM people;
+--SELECT first_name, last_name FROM people;
+
+--SELECT * FROM people WHERE state_code = 'CA';
+--SELECT * FROM people WHERE shirt_or_hat = 'shirt';
+
+--SELECT first_name, last_name FROM people
+--  WHERE shirt_or_hat = 'shirt';
+
+--SELECT first_name, last_name FROM people
+--  WHERE (state_code = 'CA' OR state_code = 'CO') AND shirt_or_hat = 'shirt' AND team is not 'Angry Ants';
+
