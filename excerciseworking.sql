6f691bf37c5febe938ec30cd940c3345e794c6aa
Trouperton <58820346+Trouperton@users.noreply.github.com>
2025-11-12 19:44:11 +0000

    Completed the course.

--- excerciseworking.sql
+++ excerciseworking.sql
@@ -153,3 +153,80 @@
   JOIN states ON people.state_code = states.state_abbrev
   GROUP BY state
   ORDER BY avgpoints DESC;*/
+
+/* ========== Chapter 4 ========== */
+
+--- Creating Values
+--INSERT INTO people (first_name) VALUES ('Bob');
+
+/*INSERT INTO people (first_name, last_name, state_code, city, shirt_or_hat)
+  VALUES ('Mary', 'Hamilton', 'OR', 'Portland', 'hat');*/
+
+/*INSERT INTO people (first_name, last_name)
+  VALUES
+    ('George', 'White'),
+    ('Jenn', 'Smith'),
+    ('Carol', NULL);*/
+
+--SELECT * FROM people;
+
+--- Updating Values
+/*SELECT last_name
+  FROM people
+  WHERE first_name = 'Carlos';
+
+UPDATE people
+  SET last_name = 'Morrison'
+  WHERE last_name = 'Morrrison';*/
+
+/*SELECT last_name
+  FROM people
+  WHERE first_name = 'Carlos';
+
+UPDATE people
+  SET last_name = 'Morrison'
+  WHERE first_name = 'Carlos' AND city = 'Houston';*/
+
+/*SELECT *
+  FROM people
+  WHERE id_number = 175;
+
+UPDATE people
+  SET last_name = 'Morrison'
+  WHERE id_number = 175;*/
+
+/*SELECT *
+  FROM people
+  WHERE company = 'Fisher LLC';
+SELECT *
+  FROM people
+  WHERE company = 'Megacorp Inc';
+
+UPDATE people
+  SET company = 'Megacorp Inc'
+  WHERE company = 'Fisher LLC';*/
+
+--- Deleting Values
+
+/*SELECT * FROM people;
+
+DELETE FROM people WHERE id_number = 1001;
+
+SELECT * FROM people;*/
+
+/*SELECT *
+  FROM people
+  WHERE quiz_points IS NULL;
+
+DELETE FROM people
+  WHERE quiz_points IS NULL;
+
+SELECT * FROM people;*/
+
+--- Practice Question
+/*SELECT first_name, last_name, state_code, team
+  FROM people
+  WHERE
+    first_name = 'Alice'
+    AND state_code = 'FL'
+    AND team = 'Cosmic Cobras';*/ --- Can be: team LIKE '%Cobras';
